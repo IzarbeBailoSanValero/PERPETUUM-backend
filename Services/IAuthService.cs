cf69c45fcca9262bb2e62149bb58fb9258489210
IzarbeBailoSanValero <a28303@svalero.com>
2026-02-05 13:57:06 +0100

    VERSION 1 PREPARADA, NO DOCKERIZADA

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -8,12 +8,11 @@ namespace PERPETUUM.Services
 {
     public interface IAuthService
     {
-        //las transformo en asincronas porque van a base de datos las dos primeras
        Task<string> LoginAsync(LoginDtoIn loginDto);
         Task<string> RegisterAsync(UserDtoIn userDto);
         public bool HasAccessToResource(int requestedUserID, ClaimsPrincipal user); //es la manera que utiliza alejandro que sirve para comprobar si con la información que hay ene l token, al descifrala, el usuario puede acceder a esos recursos , bien si son suyos o si es admin
         
-
-
+ 
+  
     }
 }
